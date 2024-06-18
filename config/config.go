f611d666e8e9094a1bc7d88941438e372c13c432
imeankenshin <omurakenshin@gmail.com>
2024-06-18 17:48:37 +0900

    envのスキーマを修正

--- config/config.go
+++ config/config.go
@@ -1,28 +1,32 @@
 package config
 
 import (
-	"github.com/ilyakaznacheev/cleanenv"
 	"log"
+
+	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/joho/godotenv"
 )
 
 type (
 	Config struct {
-		DB Database
+		DB Database `env-prefix:"DB_"`
 	}
 	Database struct {
-		Host     string `env:"DB_HOST" env-required:"true"`
-		Port     int    `env:"DB_PORT" env-required:"true"`
-		User     string `env:"DB_USER" env-required:"true"`
-		Password string `env:"DB_PASSWORD" env-required:"true"`
-		Name     string `env:"DB_NAME" env-required:"true"`
-		SslMode  string `env:"DB_SSL_MODE" env-required:"true"`
+		Host     string `env:"HOST" env-required:"true"`
+		Port     int    `env:"PORT" env-required:"true"`
+		User     string `env:"USER" env-required:"true"`
+		Password string `env:"PASSWORD" env-required:"true"`
+		Name     string `env:"NAME" env-required:"true"`
+		SslMode  string `env:"SSL_MODE" env-required:"true"`
 	}
 )
 
 func MustLoad() *Config {
 	var cfg Config
-	err := cleanenv.ReadEnv(&cfg)
-	if err != nil {
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Failed to load .env file: %v", err)
+	}
+	if err := cleanenv.ReadEnv(&cfg); err != nil {
 		log.Fatalf("Failed to load config: %v", err)
 	}
 	return &cfg
