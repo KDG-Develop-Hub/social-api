35283d4e8f337dde637d60e44694c42705c2cfd0
imeankenshin <omurakenshin@gmail.com>
2024-07-13 19:13:30 +0900

    設定ファイルとマイグレーションファイルの作成

---
+++ db/migrations/20240713183923-create-users-and-posts.sql
@@ -0,0 +1,28 @@
+
+-- +migrate Up
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(64) NOT NULL,
+    display_name VARCHAR(64) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS posts (
+    id SERIAL PRIMARY KEY,
+    content TEXT NOT NULL,
+    media_urls TEXT[] NOT NULL,
+    author_id INT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP,
+    FOREIGN KEY (author_id) REFERENCES users(id)
+);
+
+-- +migrate Down
+
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS users;
